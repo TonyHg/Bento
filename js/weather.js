163f98db930a0083612200385bd6014a68813713
Tony Heng <tony.heng@epita.fr>
2020-11-29 11:54:22 +0100

    weather: set right coordinate

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 48.815819;
+    let longitude = 2.360410;
 
     getWeather(latitude, longitude);
 }
